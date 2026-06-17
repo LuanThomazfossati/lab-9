51269d8697a2b730b3c5426436ff3e9d2a649bdf
Luan Thomaz Fossati <luangatissimo09@gmail.com>
2026-06-17 17:20:49 -0300

    Add files via upload

---
+++ lab 9/atv1.py
@@ -0,0 +1,4 @@
+x =input("Digite um seu nome: ")
+y =input("Digite seu sobrenome: ")
+nome_completo = x + y
+print(f"Bem vindo, {nome_completo}!")
---
+++ lab 9/atv2.py
@@ -0,0 +1,2 @@
+x =input("Escreva uma frase: ")
+print(f"Contagem da espaços em branco é : {x.count(' ')}")
